582bfec3dbf130aa81fcb46f0fed0c61f716035c
Igor Kashchenko <blackangryrabbit@gmail.com>
2023-10-08 16:55:59 +0300

    test task was implemented

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,39 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'usense-test-task';
+  password = '';
+
+  easyPassword = /^[A-Za-z0-9!@#$%^&*()-_=+[\]{};:'",.<>?/\\|]*$/;
+  mediumPassword = /^(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z\d]+$|^(?=.*[a-zA-Z])(?=.*[^a-zA-Z]).*$|^(?=.*[!@#$%^&*()_+=-])(?=.*\d)[!@#$%^&*()_+=-\d]+$/
+  strongPassword = /^(?=.*[a-zA-Z])(?=.*[0-9])(?=.*[!@#$%^&*()_+{}|:<>?~-]).+$/;
+
+
+  isPasswordEmpty() {
+    return this.password.length === 0;
+  }
+
+  isPasswordLongEnough() {
+    return this.password.length > 7;
+  }
+
+  sanitizePassword() {
+    this.password = this.password.replace(/\s+/g, "");
+  }
+
+  getPasswordStrength() {
+    if (!this.isPasswordLongEnough()) {
+      return 'Invalid';
+    }
+
+    switch (true) {
+      case this.strongPassword.test(this.password):
+        return 'Strong';
+      case this.mediumPassword.test(this.password):
+        return 'Medium';
+      case this.easyPassword.test(this.password):
+        return 'Easy';
+      default:
+        return;
+    }
+  }
 }
